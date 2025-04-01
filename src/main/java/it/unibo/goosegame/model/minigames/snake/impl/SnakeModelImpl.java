eccd5d6479c4eafed3f3f5d2871d806629197dab
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2025-04-01 16:41:56 +0200

    Added constructor to SnakeModelImpl

--- src/main/java/it/unibo/goosegame/model/minigames/snake/impl/SnakeModelImpl.java
+++ src/main/java/it/unibo/goosegame/model/minigames/snake/impl/SnakeModelImpl.java
@@ -1,25 +1,33 @@
 package it.unibo.goosegame.model.minigames.snake.impl;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 import it.unibo.goosegame.model.minigames.snake.api.SnakeModel;
 import it.unibo.goosegame.utilities.Direction;
+import it.unibo.goosegame.utilities.Position;
 
 public class SnakeModelImpl implements SnakeModel {
 
-    int bodyParts;
-    Random random;
-    Direction dir;
+    private static final int initSnakeLength = 3;
+    private static List<Position> snakeBody;
+    private static Random random;
+    private static Direction currentDir;
 
     public SnakeModelImpl(){
-        random = new Random();
-        changeDirection(Direction.UP);
+        this.snakeBody = new LinkedList<>();
+        for (int i = 0; i < initSnakeLength; i++) {
+            snakeBody.add(new Position(5 - i, 5));
+        }
+        this.random = new Random();
+        this.currentDir = Direction.LEFT;
     }
 
     @Override
     public void move() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'move'");
+        throw new UnsupportedOperationException("Unimplemented method 'checkCollision'");
     }
 
     @Override
@@ -37,10 +45,10 @@ public class SnakeModelImpl implements SnakeModel {
     @Override
     public void changeDirection(Direction newDirection) {
         switch(newDirection){
-            case UP: dir = Direction.UP; break;
-            case DOWN: dir = Direction.DOWN; break;
-            case LEFT: dir = Direction.LEFT; break;
-            case RIGHT: dir = Direction.RIGHT; break;
+            case UP: currentDir = Direction.UP; break;
+            case DOWN: currentDir = Direction.DOWN; break;
+            case LEFT: currentDir = Direction.LEFT; break;
+            case RIGHT: currentDir = Direction.RIGHT; break;
         }
     }
 
