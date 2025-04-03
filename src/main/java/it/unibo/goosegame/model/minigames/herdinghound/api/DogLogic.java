cb54ff9ae0835bae7fca813349e8e6a8e1f79474
Sams-Source <dambrosio.samuele2@studio.unibo.it>
2025-04-03 18:42:36 +0200

    creating DogLogic constructor

---
+++ src/main/java/it/unibo/goosegame/model/minigames/herdinghound/api/DogLogic.java
@@ -0,0 +1,20 @@
+package it.unibo.goosegame.model.minigames.herdinghound.api;
+
+import it.unibo.goosegame.model.general.MinigamesModel;
+import it.unibo.goosegame.utilities.Position;
+import java.util.Random;
+
+public interface DogLogic{
+
+    public int getX();
+
+    public int getY();
+
+    public boolean isAwake();
+
+    public int getDirection();
+
+    public void wakeUp();
+
+    public boolean canSee();
+}
