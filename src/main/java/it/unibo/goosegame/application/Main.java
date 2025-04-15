5ae0cec7923e6f26f0a8b3bb7e177e6b185dcfb1
Tedaldi Giada <giada.tedaldi@studio.unibo.it>
2025-04-15 10:06:03 +0200

    Update Main.java

--- src/main/java/it/unibo/goosegame/application/Main.java
+++ src/main/java/it/unibo/goosegame/application/Main.java
@@ -1,5 +1,12 @@
 package it.unibo.goosegame.application;
 
-public class Main {
+import javax.swing.SwingUtilities;
 
+public class Main {
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(() -> {
+      GameMenu menu = new GameMenu();
+      menu.setVisible(true);
+    });
+  }
 }
