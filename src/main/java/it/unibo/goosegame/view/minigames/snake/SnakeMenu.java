dd87285d44933fe7e3bd054997016dae6cb49797
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2025-04-15 17:58:40 +0200

    Add SnakeMenu

---
+++ src/main/java/it/unibo/goosegame/view/minigames/snake/SnakeMenu.java
@@ -0,0 +1,27 @@
+package it.unibo.goosegame.view.minigames.snake;
+
+import java.awt.event.ActionListener;
+
+//import it.unibo.goosegame.model.minigames.snake.impl.SnakeModelImpl;
+//import it.unibo.goosegame.view.MinigamesMenu.AbstractMinigameMenu;
+
+public class SnakeMenu /*extends AbstractMinigameMenu*/{
+
+    public SnakeMenu(String imgPath, String title, String infoMsg, ActionListener al) {
+        /*
+        SnakeModelImpl sm = new SnakeModelImpl();
+        super(imgPath, sm.getName(), infoMsg, al);
+
+        --Hypotetical main for Snake:
+
+         ActionListener al = e -> {
+            SnakeController.startGame();
+        };
+        new SnakeMenu("resources/backgroundSnake.png", "Snake Game", "Welcome to Snake Game! Here's some instruction to play:\\n" + //
+                        "-Move the snake using < > ^ v buttons\\n" + //
+                        "-eat 15 apples and you win\\n" + //
+                        "-hit the walls and you lose", al);
+         */
+    }
+
+}
