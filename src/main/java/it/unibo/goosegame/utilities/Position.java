c2cbbba5ca9d1938fc689ebe7b97cac0a683e1e4
Lucia Pola <luciapola@MacBook-Air-di-Lucia.local>
2025-04-16 17:47:57 +0200

    Position modified

--- src/main/java/it/unibo/goosegame/utilities/Position.java
+++ src/main/java/it/unibo/goosegame/utilities/Position.java
@@ -1,5 +1,48 @@
 package it.unibo.goosegame.utilities;
 
-public record Position(int x, int y) {
+import java.util.Objects;
+
+public class Position {
+
+    private final int x;
+    private final int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int x() {
+        return this.x;
+    }
+
+    public int y() {
+        return this.y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Position)) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Position other = (Position) obj;
+        return this.x == other.x && this.y == other.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x,y);
+    }
+
+    @Override
+    public String toString() {
+        return "Position (" + x + ", " + y + ")";
+    }
 
 }
