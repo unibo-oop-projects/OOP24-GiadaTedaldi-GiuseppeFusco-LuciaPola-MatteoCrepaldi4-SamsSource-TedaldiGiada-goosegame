1cd76701665c39284ce1f44c6e9e391b64e9b819
Lucia Pola <luciapola@MacBook-Air-di-Lucia.local>
2025-04-18 14:22:55 +0200

    Tris Controller: javadoc and interface added

---
+++ src/main/java/it/unibo/goosegame/controller/minigames/tris/api/TrisController.java
@@ -0,0 +1,27 @@
+package it.unibo.goosegame.controller.minigames.tris.api;
+
+import it.unibo.goosegame.utilities.Position;
+
+/*
+ * Interface representing the controller of a Tris(Tic-Tac-Toe) minigame.
+ */
+public interface TrisController {
+
+    /**
+     * Handles a moves made by the human player at the specified position.
+     * 
+     * @param position the position on the board where the human player wants to move
+     */
+    void movesMaker(Position position);
+
+    /**
+     * Updates the view based on the current state of the game model.
+     */
+    void updateView();
+
+    /**
+     * Ends the game disabling further input and displaying the result.
+     */
+    void endGame();
+
+}
--- src/main/java/it/unibo/goosegame/controller/minigames/tris/impl/TrisControllerImpl.java
+++ src/main/java/it/unibo/goosegame/controller/minigames/tris/impl/TrisControllerImpl.java
@@ -1,20 +1,33 @@
-package it.unibo.goosegame.controller.minigames.tris;
+package it.unibo.goosegame.controller.minigames.tris.impl;
 
+import it.unibo.goosegame.controller.minigames.tris.api.TrisController;
 import it.unibo.goosegame.model.minigames.tris.api.TrisModel;
 import it.unibo.goosegame.utilities.Position;
 import it.unibo.goosegame.view.minigames.tris.TrisView;
 
-public class TrisController {
+/**
+ * Implementation of the {@link TrisController} interface.
+ * It manages the communication between the model and the view of the Tris(Tic-Tac-Toe) minigame.
+ */
+public class TrisControllerImpl implements TrisController{
     private TrisModel model;
     private TrisView view;
     private boolean gameOver = false;
 
-    public TrisController(TrisModel model) {
+    /**
+     * Constructs a new instance of {@link TrisControllerImpl}
+     * 
+     * @param model the {@link TrisModel} representing the game logic  
+     */
+    public TrisControllerImpl(TrisModel model) {
         this.model = model;
         this.view = new TrisView(this);
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void movesMaker(Position position) {
         if(gameOver) {
             return;
@@ -32,7 +45,11 @@ public class TrisController {
 
     }
 
-    private void updateView() {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void updateView() {
         for(int i=0; i<3; i++) {
             for(int j=0; j<3; j++) {
                 Position pos = new Position(i, j);
@@ -46,7 +63,11 @@ public class TrisController {
         }
     }
 
-    private void endGame() {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void endGame() {
         gameOver=true;
         this.view.setStatus("The game is over!");
         this.view.disableButtons();
