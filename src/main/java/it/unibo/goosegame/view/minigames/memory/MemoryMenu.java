38854cdd99609cc5a80a35e773c1a3166947cea5
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2025-04-23 16:19:34 +0200

    Minor changes in MemoryMenu

--- src/main/java/it/unibo/goosegame/view/minigames/memory/MemoryMenu.java
+++ src/main/java/it/unibo/goosegame/view/minigames/memory/MemoryMenu.java
@@ -6,6 +6,18 @@ public class MemoryMenu /*extends AbstractMinigameMenu*/{
 
     public MemoryMenu(String imgPath, String title, String infoMsg, ActionListener al) {
         //super(imgPath, title, infoMsg, al);
+
+        /*
+         --Hypothetical Memory main:
+
+        ActionListener al = e -> {
+            MemoryController.startGame();
+        };
+        
+        new MemoryMenu("resources/memoryBackground.png", "Memory Game", "Welcome to Memory Game! Here's some instruction to play:\n" + //
+                        "-Find the couples beyond the cards\n" + //
+                        "-find them all and you win\n", al);
+         */
     }
 
 }
