9e3c6b53478d8ab714919aee1d191c996e7e343a
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2025-04-23 15:58:00 +0200

    Add MemoryMenu

---
+++ src/main/java/it/unibo/goosegame/view/minigames/memory/MemoryMenu.java
@@ -0,0 +1,11 @@
+package it.unibo.goosegame.view.minigames.memory;
+
+import java.awt.event.ActionListener;
+
+public class MemoryMenu /*extends AbstractMinigameMenu*/{
+
+    public MemoryMenu(String imgPath, String title, String infoMsg, ActionListener al) {
+        //super(imgPath, title, infoMsg, al);
+    }
+
+}
