43a5f552f89738c3a40de3de3a664a3512caca16
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2025-04-30 01:12:21 +0200

    change MemoryView and MemoryController

--- src/main/java/it/unibo/goosegame/view/minigames/memory/api/MemoryView.java
+++ src/main/java/it/unibo/goosegame/view/minigames/memory/api/MemoryView.java
@@ -1,15 +1,6 @@
 package it.unibo.goosegame.view.minigames.memory.api;
 
-import java.util.List;
-import java.util.Map;
-
-import javax.swing.JButton;
-
-import it.unibo.goosegame.utilities.Position;
 
 public interface MemoryView {
-
-    Map<JButton, Position> returnCells();
-
-    List<String> returnSymbols();
+    void show();
 }
--- src/main/java/it/unibo/goosegame/view/minigames/memory/impl/MemoryViewImpl.java
+++ src/main/java/it/unibo/goosegame/view/minigames/memory/impl/MemoryViewImpl.java
@@ -1,41 +1,67 @@
 package it.unibo.goosegame.view.minigames.memory.impl;
-
 import javax.swing.*;
 
+import it.unibo.goosegame.model.minigames.memory.api.MemoryModel;
 import it.unibo.goosegame.utilities.Position;
 import it.unibo.goosegame.view.minigames.memory.api.MemoryView;
 
 import java.util.*;
 import java.util.List;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
-public class MemoryViewImpl extends JPanel implements MemoryView {
+public class MemoryViewImpl implements MemoryView {
     
     private static int size = 4;
     private final List<String> symbols = new LinkedList<>(Arrays.asList("★", "♣", "☀", "⚽", "♫", "❤", "☂", "✈"));
     private final Map<JButton, Position> cells = new HashMap<>();
+    private MemoryModel model;
+    private final JFrame frame = new JFrame();
     
-    public MemoryViewImpl() {
-        this.setSize(100*size, 100*size);
-        this.setLayout(new GridLayout(size,size));            
+    public MemoryViewImpl(MemoryModel model) {
+        this.model = model;
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setTitle(model.getName());
+        this.frame.setPreferredSize(new Dimension(600, 400));
+        this.frame.setLayout(new GridLayout(size,size));  
+        this.frame.pack();
+        this.frame.setLocationRelativeTo(null);
+        this.frame.setResizable(true); 
+        ActionListener al = e -> {
+            var jb = (JButton)e.getSource();
+            this.model.hit(this.cells.get(jb));
+            this.redraw();
+            if (this.model.isOver()){
+                JOptionPane.showMessageDialog(null, model.getResult() == 1 ? "You Win!" : "You Lose...");
+                this.frame.dispose();
+            }
+        };
+        
         for (int i=0; i<size; i++){
             for (int j=0; j<size; j++){
             	final JButton jb = new JButton();
                 jb.setBackground(new Color(240, 120, 90));
                 jb.setFont(new Font("SansSerif", Font.PLAIN, 36));
                 this.cells.put(jb, new Position(j,i));
-                this.add(jb);
+                this.frame.add(jb);
             }
         }
+        this.cells.entrySet().stream().forEach(e -> e.getKey().addActionListener(al));  
     }
 
-    public List<String> returnSymbols() {
-        return List.copyOf(symbols);
-    }
+    private void redraw() {
+        for (var entry: this.cells.entrySet()){
+            entry.getKey().setText(
+                this.model.temporary(entry.getValue()).map(n -> this.symbols.get(n)).orElse(" "));
+            this.model.found(entry.getValue()).ifPresent(n -> {
+                entry.getKey().setText(this.symbols.get(n));
+                entry.getKey().setEnabled(false);
+            });
+        }
+    } 
 
     @Override
-    public Map<JButton, Position> returnCells() {
-        return Map.copyOf(this.cells);
+    public void show() {
+        this.frame.setVisible(true);
     }
-    
 }
