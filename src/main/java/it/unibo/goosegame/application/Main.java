d657d6dbe05201b252eb3df162f4ce58c6e130a9
Lucia Pola <luciapola@MacBook-Air-di-Lucia.local>
2025-05-15 16:40:16 +0200

    Minor changes

--- src/main/java/it/unibo/goosegame/application/Main.java
+++ src/main/java/it/unibo/goosegame/application/Main.java
@@ -1,20 +1,8 @@
 package it.unibo.goosegame.application;
-/* 
-import javax.swing.SwingUtilities;
-import it.unibo.goosegame.controller.minigames.tris.impl.TrisControllerImpl;
-import it.unibo.goosegame.model.minigames.tris.impl.TrisModelImpl;
-import it.unibo.goosegame.view.minigames.puzzle.impl.PuzzleViewImpl;
-*/
+
 /**
  * Main.
  */
 public class Main {
-    /* 
-    public static void main(String[] args) {
-        new TrisControllerImpl(new TrisModelImpl());
-    }
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new PuzzleViewImpl());
-    }
-    */
+
 }
