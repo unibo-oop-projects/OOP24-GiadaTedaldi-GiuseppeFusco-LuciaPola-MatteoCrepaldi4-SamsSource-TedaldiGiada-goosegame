7c025064d2f9c804a5b4a3cd4af4c23cb9f8bd8c
Tedaldi Giada <giada.tedaldi@studio.unibo.it>
2025-05-17 00:21:21 +0200

    Update HangmanControllerImpl and create interface
    fixing CheckStyle errors and formatting code

---
+++ src/main/java/it/unibo/goosegame/controller/minigames/Hangman/impl/HangmanControllerImpl.java
@@ -0,0 +1,67 @@
+package  it.unibo.goosegame.controller.minigames.hangman.impl;
+
+import java.awt.Window;
+import java.util.Objects;
+
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import it.unibo.goosegame.controller.minigames.hangman.api.HangmanController;
+import it.unibo.goosegame.model.minigames.hangman.impl.HangmanModelImpl;
+import it.unibo.goosegame.view.minigames.hangman.impl.HangmanViewImpl;
+/**
+ * This class manages the interaction between the Hangman model and view.
+ * It handles user input, updates the view based on game state,
+ * and controls the game flow, including win/lose conditions.
+ */
+public class HangmanControllerImpl implements HangmanController {
+     @SuppressFBWarnings(value = "EI2", justification = "Reference to view is safe and immutable in MVC pattern")
+    private final transient HangmanViewImpl view;
+    private final transient HangmanModelImpl model;
+
+    /**
+     * @param view the game view
+     * @param model the game model
+     */
+    public HangmanControllerImpl(final HangmanViewImpl view, final HangmanModelImpl model) {
+        this.model = Objects.requireNonNull(model);
+        this.view = Objects.requireNonNull(view);
+        init();
+    }
+
+    private void init() {
+        view.setController(this);
+        view.updateImage(model.getAttempts());
+        view.updateWord(model.getHiddenWord());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onLetterPressed(final char letter) {
+        if (model.isOver()) {
+            return;
+        }
+
+        model.guess(letter);
+        view.updateWord(model.getHiddenWord());
+        view.updateImage(model.getAttempts());
+
+        if (model.isOver()) {
+            view.disableAllButton();
+            if (model.isWon()) {
+                JOptionPane.showMessageDialog(view, "YOU WIN!");
+            } else {
+                JOptionPane.showMessageDialog(view, """
+                                                    YOU LOSE...
+                                                    The word is \t""" + model.getSelectedWord());
+            }
+            final Window window = SwingUtilities.getWindowAncestor(view);
+            if (window != null) {
+                window.dispose();
+            }
+        }
+    }
+}
