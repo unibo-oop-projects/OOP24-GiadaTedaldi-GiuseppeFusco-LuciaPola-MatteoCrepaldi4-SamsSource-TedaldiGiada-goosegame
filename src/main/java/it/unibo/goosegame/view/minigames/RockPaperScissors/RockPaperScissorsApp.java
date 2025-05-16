03b6babad71ed209cc73ea4ed2d34b7b245f78a1
Tedaldi Giada <giada.tedaldi@studio.unibo.it>
2025-05-17 00:36:59 +0200

    Delete src/main/java/it/unibo/goosegame/view/minigames/RockPaperScissors/RockPaperScissorsApp.java

--- src/main/java/it/unibo/goosegame/view/minigames/RockPaperScissors/RockPaperScissorsApp.java
+++
@@ -1,9 +0,0 @@
-package it.unibo.goosegame.view.minigames.RockPaperScissors;
-
-import javax.swing.SwingUtilities;
-
-public class RockPaperScissorsApp {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new RockPaperScissorsMenu().setVisible(true));
-    }
-}
