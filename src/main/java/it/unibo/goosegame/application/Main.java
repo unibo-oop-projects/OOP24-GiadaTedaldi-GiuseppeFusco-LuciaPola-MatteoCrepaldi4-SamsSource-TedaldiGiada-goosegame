ea03e76a10b8e83550ba3148a3bd60e590227971
Tedaldi Giada <giada.tedaldi@studio.unibo.it>
2025-05-17 00:19:36 +0200

    Update Main.java

--- src/main/java/it/unibo/goosegame/application/Main.java
+++ src/main/java/it/unibo/goosegame/application/Main.java
@@ -1,11 +1,20 @@
 package it.unibo.goosegame.application;
 
 import javax.swing.SwingUtilities;
-
-public class Main {
-  public static void main(String[] args) {
+/**
+ * Entry point of the Goose Game application.
+ */
+public final class Main {
+  private Main() {
+    throw new UnsupportedOperationException("Utility class");
+  }
+  /**
+   * 
+   * @param args
+   */
+  public static void main(final String[] args) {
     SwingUtilities.invokeLater(() -> {
-      GameMenu menu = new GameMenu();
+      final GameMenu menu = new GameMenu();
       menu.setVisible(true);
     });
   }
