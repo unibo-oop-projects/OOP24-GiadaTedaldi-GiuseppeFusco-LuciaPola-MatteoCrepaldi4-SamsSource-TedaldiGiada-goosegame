551efbc60c1a9c5440464eb5be8ed5b79cb51c20
Lucia Pola <luciapola@MacBook-Air-di-Lucia.local>
2025-05-25 15:29:44 +0200

    Added TurnManager interface and modified TurnManager in TurnManagerImpl

---
+++ src/main/java/it/unibo/goosegame/model/turnmanager/api/TurnManager.java
@@ -0,0 +1,29 @@
+package it.unibo.goosegame.model.turnmanager.api;
+
+import it.unibo.goosegame.model.player.api.Player;
+
+public interface TurnManager {
+
+    /**
+     * @return the current player 
+     */
+    public Player getCurrentPlayer();
+
+    /**
+     * Advances to the next player's turn.
+     * If a player has any turns to skip, their skip count is decreased.
+     * 
+     * @return the next player who will take their turn 
+     */
+    public Player nextTurn();
+
+    /**
+     * Increments the number of turns the specified player must skip by a given amount.
+     *
+     * @param player the player who will skip their next turn
+     * @param turns the number of turns the player will skip
+     * @throws IllegalArgumentException if the player is not part of the current player list or if turns is negative or zero
+     */
+    public void skipNextTurn(Player player, int turns);
+
+}
--- src/main/java/it/unibo/goosegame/model/turnmanager/impl/TurnManagerImpl.java
+++ src/main/java/it/unibo/goosegame/model/turnmanager/impl/TurnManagerImpl.java
@@ -1,15 +1,16 @@
-package it.unibo.goosegame.model.gameboard.impl;
+package it.unibo.goosegame.model.turnmanager.impl;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import it.unibo.goosegame.model.player.api.Player;
+import it.unibo.goosegame.model.turnmanager.api.TurnManager;
 
 /**
  * This class manages the turn order of players in game, including the ability to skip turns.
  */
-public class TurnManager {
+public class TurnManagerImpl implements TurnManager{
     private static final int MIN_PLAYERS = 2;
     private static final int MAX_PLAYERS = 4;
     private final List<Player> players;
@@ -23,7 +24,7 @@ public class TurnManager {
      * @param players the list of players participating in the game
      * @throws IllegalArgumentException if the player list is null, has fewer than 2 or more than 4 players
      */
-    public TurnManager(final List<Player> players) {
+    public TurnManagerImpl(final List<Player> players) {
         if (players == null) {
             throw new IllegalArgumentException("La lista dei giocatori non può essere nulla");
         }
