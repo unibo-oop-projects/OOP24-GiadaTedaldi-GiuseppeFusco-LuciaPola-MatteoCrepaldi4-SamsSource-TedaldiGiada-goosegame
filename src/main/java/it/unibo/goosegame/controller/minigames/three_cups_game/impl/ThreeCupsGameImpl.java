14b4fa11a7ca558128315d4777299545435c3389
matteo.crepaldi4 <matteo.crepaldi4@studio.unibo.it>
2025-06-04 17:23:54 +0200

    Refactor ThreeCupsGameImpl initialization

--- src/main/java/it/unibo/goosegame/controller/minigames/three_cups_game/impl/ThreeCupsGameImpl.java
+++ src/main/java/it/unibo/goosegame/controller/minigames/three_cups_game/impl/ThreeCupsGameImpl.java
@@ -1,6 +1,5 @@
 package it.unibo.goosegame.controller.minigames.three_cups_game.impl;
 
-import it.unibo.goosegame.controller.minigames.three_cups_game.api.MinigameEndListener;
 import it.unibo.goosegame.controller.minigames.three_cups_game.api.ThreeCupsGame;
 import it.unibo.goosegame.model.general.MinigamesModel;
 import it.unibo.goosegame.model.minigames.three_cups_game.api.ThreeCupsGameModel;
@@ -14,14 +13,13 @@ import it.unibo.goosegame.view.minigames.three_cups_game.impl.ThreeCupsGameViewI
  */
 public class ThreeCupsGameImpl implements ThreeCupsGame {
     private final ThreeCupsGameModel model;
-    private final ThreeCupsGameView view;
 
     /**
      * Construct for the Three Cups Game minigame.
      */
     public ThreeCupsGameImpl(MinigameMenu menu) {
         this.model = new ThreeCupsGameModelImpl();
-        this.view = new ThreeCupsGameViewImpl(model, menu);
+        ThreeCupsGameView view = new ThreeCupsGameViewImpl(model, menu);
 
         view.show();
     }
