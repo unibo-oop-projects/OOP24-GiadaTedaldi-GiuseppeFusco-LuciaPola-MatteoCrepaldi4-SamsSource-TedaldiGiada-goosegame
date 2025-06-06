e1319e1aa96c764ab094823e5d3faed1b685ea57
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2025-06-06 12:20:00 +0200

    Solved LookAndFeel for Mac problem

--- src/main/java/it/unibo/goosegame/application/Main.java
+++ src/main/java/it/unibo/goosegame/application/Main.java
@@ -1,6 +1,10 @@
 package it.unibo.goosegame.application;
 
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import it.unibo.goosegame.view.gamemenu.impl.GameMenu;
 /**
  * Main.
@@ -16,6 +20,15 @@ public final class Main {
    * @param args
    */
   public static void main(final String[] args) {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (final ClassNotFoundException | InstantiationException 
+             | IllegalAccessException | UnsupportedLookAndFeelException e) {
+      JOptionPane.showMessageDialog(null, 
+          "Unable to set system look and feel. Default look and feel will be used.", 
+          "Error", 
+          JOptionPane.ERROR_MESSAGE);
+    }
     SwingUtilities.invokeLater(() -> {
       final GameMenu menu = new GameMenu();
       menu.setVisible(true);
